23f1446fb49b4f65b3651292da3c6c62a475d981
guhurak <florianeven.pro@laposte.net>
2019-03-12 00:13:02 +0100

    add user in white list

--- app/controllers/tictactoe/tictactoes_controller.rb
+++ app/controllers/tictactoe/tictactoes_controller.rb
@@ -5,6 +5,7 @@ class Tictactoe::TictactoesController < ApplicationController
 	def create
 		game = Tictactoe.create(status: 0)
 		if game.save
+			TictactoeUser.create!(tictactoe_id: game.id, user_id: current_user.id)
 			redirect_to Tictactoe_path(game.id)
 		else
 			flash[:error] = "Une erreur est survenue. Si le problème persiste, contactez un admin."

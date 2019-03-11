1909201f6c378764932bdffdcf98d3d12b15c2be
guhurak <florianeven.pro@laposte.net>
2019-03-11 23:03:43 +0100

    Add methods to tictactoe controller

--- app/controllers/tictactoe/tictactoes_controller.rb
+++ app/controllers/tictactoe/tictactoes_controller.rb
@@ -1,2 +1,14 @@
 class Tictactoe::TictactoesController < ApplicationController
+	def create
+		game = Tictactoe.create(status: 0)
+		if game.save
+			redirect_to Tictactoe_path(game.id)
+		else
+			flash[:error] = "Une erreur est survenue. Si le problème persiste, contactez un admin."
+			redirect_to request.referer
+		end
+	end
+	def show
+		@party = Tictactoe.find(params[:id])
+	end
 end

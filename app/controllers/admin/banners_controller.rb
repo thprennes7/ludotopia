d2780490460e486c48b2288aff3e42e990bcc399
Montblanc159 <guillaume.zaugg@gmail.com>
2019-03-11 20:08:54 +0100

    Building admin controllers - articles, banners and donations

--- app/controllers/admin/banners_controller.rb
+++ app/controllers/admin/banners_controller.rb
@@ -1,3 +1,29 @@
 class Admin::BannersController < AdminsController
-  # Methods omitted
+  before_action :set_banner
+  respond_to :js, :html, :json
+
+
+  def index
+    @banners = Banner.all.sort.reverse
+  end
+
+  def show
+
+  end
+
+  def create
+    @banner = Banner.new(banner_params)
+    flash[:notice] = "Bannière créé !"
+    respond_with(@banner)
+  end
+
+  private
+
+  def set_banner
+    @banner = Banner.find(params[:id])
+  end
+
+  def article_params
+    params.require(:banner).permit(:title, :subtitle, :banner_imgs)
+  end
 end

496c030cb98b1b4684be674af23de80fe841dbb0
guhurak <florianeven.pro@laposte.net>
2019-03-12 00:41:19 +0100

    update model with scope

--- app/models/tictactoe_user.rb
+++ app/models/tictactoe_user.rb
@@ -1,4 +1,6 @@
 class TictactoeUser < ApplicationRecord
 	belongs_to :tictactoe
 	belongs_to :user
+
+	scope :whitelist, lambda { |user, party| where(user_id: user.id, tictactoe_id: party.id) }
 end

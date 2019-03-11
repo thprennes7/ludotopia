272e066885436481c45d3d1efa0e86e311eec9a9
yann021 <y.bonnard4@gmail.com>
2019-03-11 19:04:47 +0100

    update assets config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# ybo: add lib/assets and vendor/assets
+Rails.application.config.assets.paths << Rails.root.join('lib')
+Rails.application.config.assets.paths << Rails.root.join('vendor')

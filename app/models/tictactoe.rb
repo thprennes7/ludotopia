4c5f8fbcc8d155999c7a6d53e9d84b42ef70af68
guhurak <florianeven.pro@laposte.net>
2019-03-11 23:27:10 +0100

    simple model for tictactoe

--- app/models/tictactoe.rb
+++ app/models/tictactoe.rb
@@ -1,4 +1,13 @@
 class Tictactoe < ApplicationRecord
 	has_many :tictactoe_user
 	validate :status, persence: true
+
+
+	def create_grid
+		grid = TictactoeGrid.all
+
+		file = File.open("/db/tictactoe/party#{self.id}", "w")
+
+		file.write(grid.to_json)
+	end
 end

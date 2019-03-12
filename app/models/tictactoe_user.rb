b0df6420c5f0b81f4a4e259fe6f48165fb10f9e4
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-12 16:49:04 +0100

    add a scope if already in

--- app/models/tictactoe_user.rb
+++ app/models/tictactoe_user.rb
@@ -5,4 +5,6 @@ class TictactoeUser < ApplicationRecord
 	scope :whitelist, lambda { |user, party| find_by(user_id: user.id, tictactoe_id: party.id) }
 
 	scope :max_participant, lambda { |party| where(tictactoe_id: party).lenght < 2 }
+
+	scope :not_already_in?, lambda { |user, party| whitelist(user, party) != nil}
 end

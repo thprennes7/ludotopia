3442a57caeddc51bb49fe9463429080bceb90871
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-12 15:59:32 +0100

    add a destroy method

--- app/controllers/tictactoe/tictactoe_users_controller.rb
+++ app/controllers/tictactoe/tictactoe_users_controller.rb
@@ -1,5 +1,6 @@
 class Tictactoe::TictactoeUsersController < ApplicationController
 	before_action :authenticate_user!
+	before_action :get_participant, only: [:destroy]
 	respond_to :js, :html, :json
 
 	def create
@@ -7,7 +8,15 @@ class Tictactoe::TictactoeUsersController < ApplicationController
 		if user.save
 			respond_with(user)
 		else
-			flash[:error] = "Une erreur est survenue."
+			flash[:error] = "Une erreur est survenue lors de l'ajout."
 		end
 	end
+
+	def destroy
+		respond_with(@participant)
+	end
+
+	private
+	def get_participant
+		@particpant = TictactoeUser.find(params[:id])
 end

59f9cf51ffd182a2163e16d04d38a21353a932f3
yann021 <y.bonnard4@gmail.com>
2019-03-12 10:42:22 +0100

    call js file of template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,15 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+
+//ybo: call js file of template
+= require js/blazy.min.js
+= require js/bootstrap.min.js
+= require js/isotope.pkgd.min.js
+= require js/jquery-3.2.1.min.js
+= require js/jquery-modal-video.min.js
+= require js/lightbox.min.js
+= require js/particles.js
+= require js/popper.min.js
+= require js/strider.js
+= require js/validator.min.js
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_151701) do
+ActiveRecord::Schema.define(version: 2019_03_11_171516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,21 @@ ActiveRecord::Schema.define(version: 2019_03_11_151701) do
 
   create_table "statuses", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tictactoe_grids", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tictactoe_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tictactoes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

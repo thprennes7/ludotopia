67862b8d8a9ba0437ea33ed49f303e15c24eb7ef
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-12 11:15:33 +0100

    checkpoint

--- app/models/tictactoe_grid.rb
+++ app/models/tictactoe_grid.rb
@@ -1,11 +1,10 @@
 class TictactoeGrid < ApplicationRecord
 	def all_cases
 		# sort all cases in an array
-		table = self.all
 
 		grid = Array.new
 
-		table.each do |table_case|
+		self.each do |table_case|
 			grid << table_case.case
 		end
 

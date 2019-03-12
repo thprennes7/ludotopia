472d6e6fd91dac874639bcff46c5d583f9e64537
Samkttn <kettani.samir@outlook.fr>
2019-03-12 16:46:01 +0100

    Donations_controller are ok

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,5 @@
 class DonationsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def create
     @amount = (params[:amount] * 100).to_i

6bbbef8a79dc50f84ec63332ce056f4dbf823d60
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-12 11:43:28 +0100

    change the way to go
    juste use .select

--- app/models/tictactoe_grid.rb
+++ app/models/tictactoe_grid.rb
@@ -1,14 +1,3 @@
 class TictactoeGrid < ApplicationRecord
 
-	scope :all_cases, get_cases(all)
-
-	def get_cases(table)
-		# sort all cases in an array
-
-		table.each do |table_case|
-			grid << table_case.case
-		end
-
-		grid
-	end
 end

b3765be25b904f663ccf6c4673865efd73c94738
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-12 11:03:31 +0100

    update the grid value

--- app/models/tictactoe.rb
+++ app/models/tictactoe.rb
@@ -7,7 +7,7 @@ class Tictactoe < ApplicationRecord
 
 	def create_grid
 		#Create the JSON file for the grid of the party
-		grid = TictactoeGrid.all
+		grid = TictactoeGrid.all_cases
 
 		file = File.open("./db/tictactoe/party#{self.id}.json", "w")
 

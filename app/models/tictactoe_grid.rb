ccd98d39b5f06ca4bd1619365008378726ad2a1f
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-12 11:22:54 +0100

    add a scope

--- app/models/tictactoe_grid.rb
+++ app/models/tictactoe_grid.rb
@@ -1,4 +1,10 @@
 class TictactoeGrid < ApplicationRecord
+
+	scope :all_cases, { all.each do |case_row|
+												grid << case_row.case
+											end
+											grid }
+
 	def all_cases
 		# sort all cases in an array
 

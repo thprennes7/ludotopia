a55c3edbe05120ec3283c308fab63e06a6e32ad7
Montblanc159 <guillaume.zaugg@gmail.com>
2019-03-13 15:11:34 +0100

    Fixed merge conflict

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,39 +1,4 @@
 class DonationsController < ApplicationController
-<<<<<<< HEAD
-  before_action :set_donation, except: [:index]
-  respond_to :js, :html, :json
-
-
-  def index
-    @donations = Donation.all.sort.reverse
-    @games = Game.all.sort.reverse
-  end
-
-  def show
-
-  end
-
-  def create
-    @donation = Donation.new(donation_params)
-    flash[:notice] = "Don effectué !"
-    respond_with(@donation)
-  end
-
-  def update
-    flash[:notice] = "Donation mise à jour"
-    respond_with(@donation)
-  end
-
-  private
-
-  def set_donation
-    @donation = Donation.find(params[:id])
-  end
-
-  def article_params
-    params.require(:donation).permit(:user_id, :game_id, :stripe_customer_id, :amount)
-  end
-=======
   # before_action :authenticate_user!
 
   def create
@@ -64,5 +29,4 @@ class DonationsController < ApplicationController
   def show
     @donation = Donation.find(params[:id])
   end
->>>>>>> guillaume
 end

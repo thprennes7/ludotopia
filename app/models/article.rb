c767311b938719eff8c38501dc88386ce8a0b753
Montblanc159 <guillaume.zaugg@gmail.com>
2019-03-13 18:50:49 +0100

    fix merge conflicts

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,8 @@
 class Article < ApplicationRecord
   has_many :comments
   has_many :likes
-<<<<<<< HEAD
   has_many_attached :article_images
-
-=======
-  has_many_attached :article_imgs
   after_create :send_news
->>>>>>> master
 
   private
 

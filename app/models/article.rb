f3ca96169321363917ec1e4a55969fdfd241387c
Montblanc159 <guillaume.zaugg@gmail.com>
2019-03-13 16:12:31 +0100

    WIP article index

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   after_create :send_news
   has_many :comments
   has_many :likes
-  has_many_attached :article_imgs
+  has_many_attached :article_images
 
 
   private

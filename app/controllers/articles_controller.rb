6a2bc162ee7d58aa4c53a03f652a8b372ddb4054
aurelien <duquenneaurelien@gmail.com>
2019-03-13 16:14:20 +0100

    method create, finaly delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def create
+    @article = Article.new(title: params[:title], description: params[:description])
+    if @article.save
+      redirect_to root_path
+    end
+  end
 end

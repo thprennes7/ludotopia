58a094843f527172ea24b1dbe43145e23830181d
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-14 11:13:51 +0100

    modify the table

--- db/migrate/20190311171516_create_tictactoe_grids.rb
+++ db/migrate/20190311171516_create_tictactoe_grids.rb
@@ -1,7 +1,17 @@
 class CreateTictactoeGrids < ActiveRecord::Migration[5.2]
   def change
     create_table :tictactoe_grids do |t|
-    	t.integer :case
+    	t.belongs_to :tictactoe, index: true
+    	t.string :a1
+    	t.string :a2
+    	t.string :a3
+    	t.string :b1
+    	t.string :b2
+    	t.string :b3
+    	t.string :c1
+    	t.string :c2
+    	t.string :c3
+    	t.integer :player
       t.timestamps
     end
   end

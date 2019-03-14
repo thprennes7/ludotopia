3be4548a06b766be26aadcd3009f3718df20df22
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-14 11:15:46 +0100

    remove aftercreate method

--- app/models/tictactoe.rb
+++ app/models/tictactoe.rb
@@ -3,17 +3,5 @@ class Tictactoe < ApplicationRecord
 	has_one :tictactoe_grid
 	validates :status, presence: true
 
-	after_create :create_grid
-
-
-	def create_grid
-		#Create the JSON file for the grid of the party
-		grid = TictactoeGrid.select(:id, :case)
-
-		file = File.open("./db/tictactoe/party#{self.id}.json", "w")
-
-		file.write('{"grid": ' + grid.to_json + ', "player": "1"}')
-
-		file.close
-	end
+	
 end

e08f2870292df3685794d47342446e6749a5c616
guhurak <florianeven.pro@laposte.net>
2019-03-15 11:57:36 +0100

    add conditions for update

--- app/controllers/tictactoe/tictactoe_grids_controller.rb
+++ app/controllers/tictactoe/tictactoe_grids_controller.rb
@@ -5,6 +5,34 @@ class Tictactoe::TictactoeGridsController < ApplicationController
 		else
 			next_player = 1
 		end
-		TictactoeGrid.find_by(tictactoe_id: params[:id]).update(player: next_player, a1: params[:a1], a2: params[:a2], a3: params[:a3], a4: params[:a4], a5: params[:a5], a6: params[:a6], a7: params[:a7], a8: params[:a8], a9: params[:a9])
+		grid = TictactoeGrid.find_by(tictactoe_id: params[:id])
+		grid.update(player: player)
+		if params[:a1] != nil
+			grid.update(a1: params[:a1])
+		end
+		if params[:a2] != nil
+			grid.update(a2: params[:a2])
+		end
+		if params[:a3] != nil
+			grid.update(a3: params[:a3])
+		end
+		if params[:a4] != nil
+			grid.update(a4: params[:a4])
+		end
+		if params[:a5] != nil
+			grid.update(a5: params[:a5])
+		end
+		if params[:a6] != nil
+			grid.update(a6: params[:a6])
+		end
+		if params[:a7] != nil
+			grid.update(a7: params[:a7])
+		end
+		if params[:a8] != nil
+			grid.update(a8: params[:a8])
+		end
+		if params[:a9] != nil
+			grid.update(a9: params[:a9])
+		end
 	end
 end

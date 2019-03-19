9d5ec3b18a2495c767bdb3e37a6298fe71bdffa1
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-19 18:48:50 +0100

    get all factories ready

--- spec/factories/statuses.rb
+++ spec/factories/statuses.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :status do
-    
+    name {"Membre"}
+  end
+  factory :status do
+  	name {"Admin"}
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name {"MyString"}
+    last_name {"MyString"}
+    username {"MyString"}
+    email {"MyString"}
+    password {"MyString"}
+    status_id {1}
+  end
+  factory :user do
+    first_name {"MyString"}
+    last_name {"MyString"}
+    username {"MyString"}
+    email {"MyString"}
+    password {"MyString"}
+    status_id {2}
   end
 end

2f924a056a76a09895f1b08872d8cdf40582fb68
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-20 10:35:26 +0100

    remove second user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,12 +7,4 @@ FactoryBot.define do
     password {"MyString"}
     status_id {1}
   end
-  factory :user do
-    first_name {"MyString"}
-    last_name {"MyString"}
-    username {"MyString"}
-    email {"MyString"}
-    password {"MyString"}
-    status_id {2}
-  end
 end

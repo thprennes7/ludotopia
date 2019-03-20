b6a68ceaa11d88346f5764fbb273e1b5a64c9738
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-20 16:04:25 +0100

    modify create method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,8 +2,12 @@ class ContactsController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		user = User.find_by(username: params[:user])
-		if Contact.find_by(me: current_user, friend: user) && user != current_user
-			contact = Contact.create(me: current_user, friend: user)
+		unless Contact.find_by(me: current_user, friend: user) || user == current_user
+			@contact = Contact.create(me: current_user, friend: user)
+		end
+		respond_to do |format|
+			format.js
+			format.html
 		end
 	end
 

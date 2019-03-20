49f95a6b2a225c9c6273aa1b727cb395854780a0
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-20 11:30:55 +0100

    update some method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,13 @@
 class ContactsController < ApplicationController
 	def create
-		if Contact.find_by(me: current_user, friend: params[:user])
+		if Contact.find_by(me: current_user, friend: params[:user]) && params[:user] != current_user
 			contact = Contact.create(me: current_user, friend: params[:user])
 		end
 	end
 
 	def index
-		friends = Contact.where(me: current_user)
+		@users = User.all
+		@friends = Contact.where(me: current_user)
 	end
 
 	def destroy

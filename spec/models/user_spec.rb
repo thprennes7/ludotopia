24b1dca38b48d2d08cdcb1120f6f3b693e6e875d
yann021 <y.bonnard4@gmail.com>
2019-03-20 18:04:54 +0100

    association test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before(:all) do
-  	@user = FactoryBot.create(:user)
+
+  	@user    = FactoryBot.create(:user)
+  	@article  = FactoryBot.create(:article)
+  	@comment  = FactoryBot.create(:comment)
+  	@game     = FactoryBot.create(:game)
+   	@donation = FactoryBot.create(:donation)
+  	@like     = FactoryBot.create(:like)
+  	@score    = FactoryBot.create(:score)
   end
 
-context "validation" do
-    it "is valid with valid attributes" do
-      expect(@user).to be_a(User)
-      expect(@user).to be_valid
-    end
-end
+	context "validation" do
+	    it "is valid with valid attributes" do
+	      expect(@user).to be_a(User)
+	      expect(@user).to be_valid
+	    end
+	end
 
+  context 'associations' do
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
+    it { should have_many(:scores) }
+    it { should have_many(:donations) }
+    it { should have_many(:games).through(:scores) } 
+  end
 
 end

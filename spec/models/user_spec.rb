725461a90a8321282267d52207c4e7601fed3e6d
yann021 <y.bonnard4@gmail.com>
2019-03-20 17:03:26 +0100

    validtion test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+  	@user = FactoryBot.create(:user)
+  end
+
+context "validation" do
+    it "is valid with valid attributes" do
+      expect(@user).to be_a(User)
+      expect(@user).to be_valid
+    end
+end
+
+
 end

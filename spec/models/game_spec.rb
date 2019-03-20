b5e12664d5856ddfedebe5a7820e904588a1f84a
yann021 <y.bonnard4@gmail.com>
2019-03-20 20:08:00 +0100

    validation test for Games

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+  	@user     = FactoryBot.create(:user)
+  	@article  = FactoryBot.create(:article)
+  	@comment  = FactoryBot.create(:comment)
+  	@game     = FactoryBot.create(:game)
+   	@donation = FactoryBot.create(:donation)
+  	@like     = FactoryBot.create(:like)
+  	@score    = FactoryBot.create(:score)
+  end
+
+  context "validation" do
+	    it "is valid with valid attributes" do
+	      expect(@game).to be_a(Game)
+	      expect(@game).to be_valid
+	    end
+	end
+
 end

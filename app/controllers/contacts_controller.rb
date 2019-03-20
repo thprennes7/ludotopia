f6b2858080dddabfe36d591091be4dcb833ffe1f
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-20 11:09:57 +0100

    add some method to the contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,15 @@
 class ContactsController < ApplicationController
+	def create
+		if Contact.find_by(me: current_user, friend: params[:user])
+			contact = Contact.create(me: current_user, friend: params[:user])
+		end
+	end
+
+	def index
+		friends = Contact.where(me: current_user)
+	end
+
+	def destroy
+		contact = Contact.find_by(me: current_user, friend: params[:user])
+	end
 end

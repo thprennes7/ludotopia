a2792f59a0ba058e0c753c9990fdca6a168695ab
guhurak <45952972+guhurak@users.noreply.github.com>
2019-03-21 14:36:28 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -170,6 +170,7 @@ ActiveRecord::Schema.define(version: 2019_03_21_104643) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["status_id"], name: "index_users_on_status_id"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"

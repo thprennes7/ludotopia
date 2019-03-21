dc99aa78a4167d9436a12774edb2e3b1d171f946
yann021 <y.bonnard4@gmail.com>
2019-03-21 22:40:33 +0100

    generate HomeTab model

---
+++ app/models/home_tab.rb
@@ -0,0 +1,2 @@
+class HomeTab < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_104643) do
+ActiveRecord::Schema.define(version: 2019_03_21_213816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,11 @@ ActiveRecord::Schema.define(version: 2019_03_21_104643) do
     t.string "title"
     t.text "description"
     t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "home_tabs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

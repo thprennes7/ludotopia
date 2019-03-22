6eaaccbdbffc9654bac5e2f0cbfe748ae52e9818
Samkttn <kettani.samir@outlook.fr>
2019-03-22 15:33:11 +0100

    Add alert for bad email

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -5,7 +5,9 @@ class SupportsController < ApplicationController
 
   def create
     @support = Support.new(support_params)
-    flash[:notice] = "Message envoyé !" if @support.save flash[:danger] = "email invalide"
+    flash[:notice] = "Message envoyé !" if @support.save
+    else
+      flash[:alert] = "email invalide"
 
     redirect_to root_path
   end
